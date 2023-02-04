ff018d9e318e5c69dd3dfc9d66217c55dd449340
Retired Tolu <retoluf@gmail.com>
2023-02-04 14:06:50 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1536px',
+    },
+    extend: {
+      backgroundImage: {
+        'lambo': "url('../assets/images/millennium_lambo.png')",
+        'luxury': "url('../assets/images/millennium_luxury.png')",
+        'yatching': "url('../assets/images/millennium_yatch.png')",
+        'jet': "url('../assets/images/millennium_jet.png')",
+        'igNewYear': "url('../assets/images/ig_newyear.jpg')",
+        'igNewMonth': "url('../assets/images/ig_newmonth.jpg')",
+        'igLusry': "url('../assets/images/ig_lusry.jpg')",
+        'igFerrari': "url('../assets/images/ig_ferrari.jpg')",
+        'igYatch': "url('../assets/images/ig_yatch.jpg')",
+      },
+      colors: {
+        main: '#1E1E1E',
+        paleGold: '#B6862B',
+        veryPaleGold: '#BF9A53',
+        lightPaleGold: '#D8A950',
+        veryLightPaleGold: '#FFE6B7',
+        milkyWay: '#DFBF81',
+        gradi: '#D8A950',
+        milk: '#D9D9D9'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        allenoire: ["ALLENOIRE", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+
+}
